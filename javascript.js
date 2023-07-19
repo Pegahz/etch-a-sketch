425fc1c43b9fc31d240030f76affe608c8ce97c4
Pegahz <zahra.javadipour1@gmail.com>
2023-07-19 06:52:01 -0700

    Add function to paint

--- javascript.js
+++ javascript.js
@@ -1,18 +1,29 @@
-// this function creates a 16 * 16 grid by creating 16 rows and adding 16 cells to each row
-function createGrid() {
+// this function creates a size * size grid by creating #size rows and adding #size cells to each row
+function createGrid(size = 16) {
     const container = document.querySelector('.container');
-    for (let rows = 0; rows < 16; rows++) {
+    for (let rows = 0; rows < size; rows++) {
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        for (let columns = 0; columns < 16; columns++) {
+        for (let columns = 0; columns < size; columns++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.style.width = 100 / 16 + '%';
-            cell.style.paddingTop = 100 / 16 + '%';// aspect ratio
+            cell.style.width = 100 / size + '%';
+            cell.style.paddingTop = 100 / size + '%';// aspect ratio
             row.appendChild(cell);
         }
     }
 }
 
+// this function paints the cells black if the mouse hovers over them
+function paintCells () {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });
+    });
+}
+
 createGrid();
+paintCells();
